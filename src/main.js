7b2b3028b82876a501f9ca62d14c6286a49080a0
Bruce <bruce_ly@qq.com>
2018-10-23 15:47:25 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './css/index.css'
 import Toast from './js/index.js'
 
-export function toast (text, options) {
+export default function toast(text, options) {
     return new Toast(text, options);
 }
